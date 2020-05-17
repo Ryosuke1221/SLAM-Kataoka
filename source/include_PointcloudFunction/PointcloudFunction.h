e7346219e80937b62e828405ea65cf224ca74d03
Ryosuke Kataoka <cat1212cut@gmail.com>
2020-05-18 02:24:22 +0900

    complate viewer

--- source/include_PointcloudFunction/PointcloudFunction.h
+++ source/include_PointcloudFunction/PointcloudFunction.h
@@ -2,7 +2,6 @@
 
 #include<iostream>
 #include <vector>
-#include<windows.h>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -11,8 +10,12 @@
 #include <pcl/registration/transforms.h>
 #include <pcl/visualization/cloud_viewer.h>
 
+//should be under pcl includes
+#include<windows.h>
+
 
 #define M_PI 3.14159265359
+
 
 using namespace std;
 //typedef pcl::PointXYZI PointType;
@@ -26,31 +29,11 @@ namespace Eigen {
 
 }    // namespace Eigen
 
-template < typename T_PointType >	class CPointVisualization
-{
-
-	pcl::visualization::PointCloudColorHandler<T_PointType>::Ptr m_handler;
-	boost::shared_ptr<pcl::visualization::PCLVisualizer> m_viewer;				//ëÂè‰ïvÅH
-	string name_window;
-
-public:
-	//template < typename T_PointType >
-	//CPointVisualization();
-
-	CPointVisualization();
-
-	void setViewer(const string name_window_arg);
-
-
-	//m_viewer->close();
-};
-
-
 class CPointcloudFuction
 {
 
 public:
-	CPointcloudFuction() 
+	CPointcloudFuction()
 	{
 
 	}
@@ -72,3 +55,134 @@ public:
 	//CPointcloudFuction::CPointVisualization<pcl::PointXYZ> pointv4("test");
 
 };
+
+template <class T_PointType>
+class CPointVisualization
+{
+	//https://stackoverflow.com/questions/40544174/class-templating-to-process-pclpointcloud-objects-with-different-point-types
+	typedef typename pcl::visualization::PointCloudColorHandler<T_PointType> T_ColorHandler;
+	typedef typename pcl::PointCloud<T_PointType>::Ptr T_PointCloudPtr;
+	typedef typename pcl::visualization::PointCloudColorHandlerCustom<T_PointType> T_ColorHandlerCustom;				//XYZ
+	typedef typename pcl::visualization::PointCloudColorHandlerGenericField<T_PointType> T_ColorHandlerGenericField;	//XYZI
+	typedef typename pcl::visualization::PointCloudColorHandlerRGBField<T_PointType> T_ColorHandlerRGBField;			//XYZRGB
+
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> M_viewer;
+	boost::shared_ptr<T_ColorHandler> M_handler;
+	string M_name_window;
+	boost::mutex M_mutex;
+	T_PointCloudPtr M_cloud_;
+	T_PointType M_point;
+
+public:
+
+	CPointVisualization();
+
+	void setWindowName(const string name_window_arg);
+
+	void setPointCloud(T_PointCloudPtr cloud_arg);
+	void updateViewer();
+
+};
+
+//int main()
+//{
+//	string foldername_;
+//	foldername_ = "../../data/temp";
+//	string s_filename_PC;
+//	s_filename_PC = foldername_ + "/000.pcd";
+//	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_1(new pcl::PointCloud<pcl::PointXYZ>());
+//	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_2(new pcl::PointCloud<pcl::PointXYZI>());
+//	pcl::io::loadPCDFile(s_filename_PC, *cloud_1);
+//	pcl::io::loadPCDFile(s_filename_PC, *cloud_2);
+//	CPointVisualization<pcl::PointXYZ> pv1;
+//	CPointVisualization<pcl::PointXYZI> pv2;
+//	pv1.setWindowName("show XYZI");
+//	pv2.setWindowName("show XYZI2");
+//	while (1)
+//	{
+//		pv1.setPointCloud(cloud_1);
+//		pv1.updateViewer();
+//		pv2.setPointCloud(cloud_2);
+//		pv2.updateViewer();
+//	}
+//	cout << "finish" << endl;
+//	return 0;
+//}
+
+//https://qiita.com/i153/items/38f9688a9c80b2cb7da7
+template < typename T_PointType >
+CPointVisualization<T_PointType>::CPointVisualization()
+{
+	setWindowName("init");
+	//m_viewer->removeCoordinateSystem("coordinate");		//remove axis in viewer
+
+	// Point Cloud Color Handler
+	const std::type_info& type = typeid(T_PointType);
+	if (type == typeid(pcl::PointXYZ)) 
+	{
+		std::vector<double> color = { 255.0, 255.0, 255.0 };
+		boost::shared_ptr<T_ColorHandlerCustom> color_handler(new T_ColorHandlerCustom(color[0], color[1], color[2]));
+		M_handler = color_handler;
+	}
+	else if (type == typeid(pcl::PointXYZI)) 
+	{
+		boost::shared_ptr<T_ColorHandlerGenericField> color_handler(new T_ColorHandlerGenericField("intensity"));
+		M_handler = color_handler;
+	}
+	else if (type == typeid(pcl::PointXYZRGB)) {
+		boost::shared_ptr<T_ColorHandlerRGBField> color_handler(new T_ColorHandlerRGBField());
+		M_handler = color_handler;
+	}
+	//else if (type == typeid(pcl::PointXYZRGB)) {
+	//	boost::shared_ptr<T_ColorHandlerCustom> color_handler(new T_ColorHandlerCustom());
+	//	M_handler = color_handler;
+	//}
+	//else if (type == typeid(pcl::PointXYZRGBA)) {
+	//	boost::shared_ptr<T_ColorHandlerCustom> color_handler(new T_ColorHandlerCustom());
+	//	M_handler = color_handler;
+	//}
+	else {
+		throw std::runtime_error("This PointType is unsupported.");
+	}
+
+	M_cloud_ = (new pcl::PointCloud<T_PointType>)->makeShared();
+}
+
+template < typename T_PointType >
+void CPointVisualization<T_PointType>::setWindowName(const string name_window_arg)
+{
+	if(M_viewer) M_viewer->close();
+	M_viewer.reset(new pcl::visualization::PCLVisualizer(name_window_arg));
+	M_viewer->addCoordinateSystem(3.0, "coordinate");
+	M_viewer->setBackgroundColor(0.0, 0.0, 0.0, 0);
+	M_viewer->initCameraParameters();
+	M_viewer->setCameraPosition(0.0, 0.0, 30.0, 0.0, 1.0, 0.0, 0);
+}
+
+template < typename T_PointType >
+void CPointVisualization<T_PointType>::setPointCloud(T_PointCloudPtr cloud_arg)
+{
+	//mutex with scoped_lock
+	boost::mutex::scoped_lock lock(M_mutex);
+	//update pointcloud
+	pcl::copyPointCloud(*cloud_arg, *M_cloud_);
+}
+
+template < typename T_PointType >
+void CPointVisualization<T_PointType>::updateViewer()
+{
+	if (!M_viewer->wasStopped())
+	{
+		M_viewer->spinOnce();
+
+		//mutex with no stop
+		boost::mutex::scoped_try_lock lock(M_mutex);
+		if (lock.owns_lock() && M_cloud_)
+		{
+			M_handler->setInputCloud(M_cloud_);
+			//update viewer
+			if (!M_viewer->updatePointCloud(M_cloud_, *M_handler, "cloud"))
+				M_viewer->addPointCloud(M_cloud_, *M_handler, "cloud");
+		}
+	}
+}
